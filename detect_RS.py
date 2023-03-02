2d91ce124cecc25ba9431d5dbbc4b5463fde5e0c
SD on Jetson <stiven.ding@live.hk>
2023-03-02 22:37:40 +0800

    update

--- detect_RS.py
+++ detect_RS.py
@@ -19,6 +19,32 @@ if ENABLE_YOLO_DETECT:
     from ncnn.utils import draw_detection_objects
     from yolov8 import YoloV8s
 
+class Obstable():
+    def __init__(self, dist, xyxy, name, prob, mid_xy):
+        self.dist = dist
+        self.xyxy = xyxy
+        self.name = name
+        self.prob = prob
+        self.mid_xy = mid_xy
+  
+    def __lt__(self, obj):
+        return ((self.dist) < (obj.dist))
+  
+    def __gt__(self, obj):
+        return ((self.dist) > (obj.dist))
+  
+    def __le__(self, obj):
+        return ((self.dist) <= (obj.dist))
+  
+    def __ge__(self, obj):
+        return ((self.dist) >= (obj.dist))
+  
+    def __eq__(self, obj):
+        return (self.dist == obj.dist)
+
+    def __repr__(self):
+        return '{' + str(self.dist) + ', ' + self.dist + ', ' + self.prob + '}'
+
 
 def sample_distance(depth_image, mid_x, mid_y):
     global depth_scale
@@ -30,7 +56,7 @@ def sample_distance(depth_image, mid_x, mid_y):
 
     return dist
 
-def draw_label(image, text, x0, y0, x1, y1):
+def draw_label(image, text, x0, y0, x1, y1, color=(0,0,0)):
 
     cv2.rectangle(
         image,
@@ -52,7 +78,7 @@ def draw_label(image, text, x0, y0, x1, y1):
         image,
         (int(x), int(y)),
         (int(x + label_size[0]), int(y + label_size[1] + baseLine)),
-        (255, 255, 255),
+        color,
         -1,
     )
 
@@ -60,34 +86,27 @@ def draw_label(image, text, x0, y0, x1, y1):
         image,
         text,
         (int(x), int(y + label_size[1])),
-        cv2.FONT_HERSHEY_SIMPLEX,
+        cv2.LINE_AA,
         0.5,
-        (0, 0, 0),
+        (255, 255, 255),
     )
 
+def draw_detection_objects(image, depth_colormap, objects):
+    for idx, obj in enumerate(objects):
+        x0, y0, x1, y1 = obj.xyxy[0], obj.xyxy[1], obj.xyxy[2], obj.xyxy[3]
+        text = f'{obj.name} {obj.dist:.2f}m'
 
-def draw_detection_objects(image, depth_image, class_names, objects, min_prob=0.0):
-    for obj in objects:
-        if obj.prob < min_prob:
-            continue
+        if idx < 2:
+            color = (50,50,150)
+        else:
+            color = (0,150,100) if obj.prob < 0 else (0,0,0)
 
-        #print(
-        #    "%d = %.5f at %.2f %.2f %.2f x %.2f"
-        #    % (obj.label, obj.prob, obj.rect.x, obj.rect.y, obj.rect.w, obj.rect.h)
-        #)
-
-        x0, y0, x1, y1 = obj.rect.x, obj.rect.y, obj.rect.x+obj.rect.w, obj.rect.y+obj.rect.h
-        mid_x, mid_y = round(int(x0+x1) /2), round(int(y0 + y1)/2)
-
-        dist = sample_distance(depth_image, mid_x, mid_y)
-        name = class_names[int(obj.label)]
-        conf = obj.prob * 100
-
-        text = f'{name} {dist:.2f}m {conf:.0f}%'
-
-        draw_label(image, text, x0, y0, x1, y1)
+        draw_label(image, text, x0, y0, x1, y1, color)
+        draw_label(depth_colormap, text, x0, y0, x1, y1, color)
 
 def detect(save_img=False):
+
+    t0 = time.perf_counter()
 
     global device, model, half, depth_scale
     #source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
@@ -100,10 +119,10 @@ def detect(save_img=False):
     if ENABLE_YOLO_DETECT:
         print('Loading model')
         net = YoloV8s(
-            target_size=412,
+            target_size=32*6,
             prob_threshold=0.25,
             nms_threshold=0.45,
-            num_threads=4,
+            num_threads=2,
             use_gpu=True,
         )
         
@@ -115,6 +134,7 @@ def detect(save_img=False):
     #for dev in devices:
     #    dev.hardware_reset()
     #print("RS reset done")
+
     print('Enabling RS camera')
     pipeline = rs.pipeline()
     profile = pipeline.start(config)
@@ -132,8 +152,13 @@ def detect(save_img=False):
     align_to = rs.stream.color
     align = rs.align(align_to)
 
+    t1 = time.perf_counter()
+    print(f' ({(1E3 * (t1 - t0)):.1f}ms) boot\n')
+
+
     while True:
-        #t0 = time.time()
+        t0 = time.perf_counter()
+
         while True:
             try:
                 frames = pipeline.wait_for_frames()
@@ -149,13 +174,15 @@ def detect(save_img=False):
             continue
 
         # filtering
+        t1 = time.perf_counter()
+
         #depth_to_disparity = rs.disparity_transform(True)
         #disparity_to_depth = rs.disparity_transform(False)
-        spatial = rs.spatial_filter(smooth_alpha=1,smooth_delta=50,magnitude=5,hole_fill=3)
+        #spatial = rs.spatial_filter(smooth_alpha=1,smooth_delta=50,magnitude=5,hole_fill=3)
         hole_filling = rs.hole_filling_filter(1)
 
         #depth_frame = depth_to_disparity.process(depth_frame)
-        depth_frame = spatial.process(depth_frame)
+        #depth_frame = spatial.process(depth_frame)
         #depth_frame = disparity_to_depth.process(depth_frame)
         depth_frame = hole_filling.process(depth_frame)
 
@@ -170,22 +197,14 @@ def detect(save_img=False):
         depth_img = cv2.medianBlur(depth_img,5)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_img, alpha=0.03), cv2.COLORMAP_JET)
 
-        # Depth range selection
-        #depth_l1_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_l1, alpha=0.2), cv2.COLORMAP_JET)
-        #depth_l2 = cv2.inRange(depth_image,0.5/depth_scale, 1.0/depth_scale)
-        #depth_l2_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_l2, alpha=0.2), cv2.COLORMAP_JET)
-        #contours_l2, _ = cv2.findContours(depth_l2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        #
+        # All obstacles list
+        obstacles = []
+
 
         # Process contours
-        #edged = cv2.Canny(depth_img.astype(np.uint8), 50, 200)
-        #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,15))
-        #dilate = cv2.dilate(edged, kernel, iterations =1)
-        #contours, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #cv2.drawContours(depth_colormap, contours, -1, (0,255,0), 2)
-
+        t2 = time.perf_counter()
         contours = []
-        c_start, c_step, c_levels = 0.0, 0.5, 3
+        c_start, c_step, c_levels = 0.0, 0.5, 5
         for i in range(c_levels):
             depth_range = cv2.inRange(depth_img,c_start/depth_scale, (c_start+c_step)/depth_scale)
             contours_range, _ = cv2.findContours(depth_range,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -199,32 +218,48 @@ def detect(save_img=False):
             size = cv2.contourArea(c)
             x,y,w,h = cv2.boundingRect(c)
 
-            if w > 50 and h > 50 and w < 640 and h < 480:
-                cv2.rectangle(depth_colormap, (x,y), (x+w,y+h), (0,200,100),2)
-                M = cv2.moments(c)
-                mid_x, mid_y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+            if w < 50 or h < 50 or w >= 640 or h >= 480:
+                continue
+        
+            M = cv2.moments(c)
+            mid_x, mid_y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+            dist = sample_distance(depth_img, mid_x, mid_y)
+
+            obs = Obstable(dist, [x, y, x+w, y+h], '???', -1, [mid_x, mid_y])
+            obstacles.append(obs)
+
+
+        # YOLO inference
+        t3 = time.perf_counter()
+        if ENABLE_YOLO_DETECT:
+            objects = net(img)
+            
+            for obj in objects:
+                if obj.prob < 0.1:
+                    continue
+
+                xyxy = [obj.rect.x, obj.rect.y, obj.rect.x+obj.rect.w, obj.rect.y+obj.rect.h]
+                mid_x, mid_y = round(int(xyxy[0]+xyxy[2]) /2), round(int(xyxy[1] + xyxy[3])/2)
 
                 dist = sample_distance(depth_img, mid_x, mid_y)
-                cv2.putText(depth_colormap, "dist: " + str(round(dist,2)) + "m", 
-                (x, y-7),
-                cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,200,0),2)
+                name = net.class_names[int(obj.label)]
 
-        if ENABLE_YOLO_DETECT:
-            # Inference
-            #t1 = time_synchronized()
-            objects = net(img)
-            #t2 = time_synchronized()
+                obs = Obstable(dist, xyxy, name, obj.prob, [mid_x, mid_y])
+                obstacles.append(obs)
 
-            # Process detections
-            draw_detection_objects(im0, depth_img, net.class_names, objects)
+        # Process detections
+        draw_detection_objects(im0, depth_colormap, obstacles)
+        obstacles.sort()
 
-            # Print time (inference + NMS)
-            #print(f' ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+        t4 = time.perf_counter()
+        
+        # Print time (inference + NMS)
+        print(f' ({(1E3 * (t1 - t0)):.1f}ms) input, ({(1E3 * (t2 - t1)):.1f}ms) filter, ({(1E3 * (t3 - t2)):.1f}ms) cont, ({(1E3 * (t4 - t3)):.1f}ms) yolo, ({(1E3 * (t4 - t0)):.1f}ms) total')
 
         # Stream results
-        cv2.imshow("YOLOv7 result", im0)
+        cv2.imshow("YOLOv8 result", im0)
         #cv2.imshow("Depth L1 result",cv2.cvtColor(depth_l1_colormap, cv2.COLOR_BGR2RGB))
-        cv2.imshow("L result depth",cv2.cvtColor(depth_colormap, cv2.COLOR_BGR2RGB))
+        cv2.imshow("L result depth", cv2.cvtColor(depth_colormap, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
